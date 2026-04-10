00586dc87263bed8f1191fa50b055f08919f787e
d11416214-dotcom <d11416214@gs.takming.edu.tw>
2026-04-10 10:53:43 +0800

    Add files via upload

---
+++ D11416214_H5/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person();
+            Console.WriteLine(p.name);
+            Person pl=new Person("帶入後參數的名字");
+            Console.WriteLine(pl.name);
+            Student std1=new Student("主程式視帶入參數的名字");
+            Console.WriteLine(std1.name);
+        }
+
+        public class Person
+        {
+            public string name;
+            public Person()
+            {
+                name = "Person的名字";
+            }
+            public Person(string name)
+            {
+                this.name = name;
+            }
+        }
+
+        public class Student:Person
+        {
+
+            static string name = "Std靜態資料型別預設";
+            public Student():base(name)
+            {
+
+            }
+            public Student(string name) : base(name)
+            {
+
+            }
+        }
+    }
+}
